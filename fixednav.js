02e54f8574e97ce4654b14e50056c86005a9ea03
bawinsl2 <bawinsl2@ncsu.edu>
2017-05-05 00:41:37 -0400

    formatted sections, added dropdowns

--- fixednav.js
+++ fixednav.js
@@ -1,23 +1,42 @@
 $(function() {
-    $("a").on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-          // Store hash
-          var hash = this.hash;
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        } // End if
-    });
+    // $("a").on('click', function(event) {
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //       // Prevent default anchor click behavior
+    //       event.preventDefault();
+    //       // Store hash
+    //       var hash = this.hash;
+    //       // Using jQuery's animate() method to add smooth page scroll
+    //       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //       $('html, body').animate({
+    //         scrollTop: $(hash).offset().top
+    //       }, 800, function(){
+    //         // Add hash (#) to URL when done scrolling (default click behavior)
+    //         window.location.hash = hash;
+    //       });
+    //     } // End if
+    // });
 
-    $("#menu-icon").click(function(){
-      $(".mobile-nav").toggle();
-    })
+    // $("#menu-icon").click(function(){
+    //   $(".mobile-nav").toggle();
+    // })
+
+
+    $("#example-nav").click(function(e){
+        e.stopPropagation();
+        $(".dropdown").slideToggle("fast");
+    });
+     $(".mobile-menu").click(function(e){
+        e.stopPropagation();
+        $(".mobile-dropdown").slideToggle("fast");
+        // $(".mobile-dropdown").animate({width: 'toggle'});
+    });
+    $(document).on("click", function(event){
+        $(".dropdown").slideUp("fast");
+        $(".mobile-dropdown").slideUp("fast");
+        // if($(".mobile-dropdown").is(":visible")){
+        //   $(".mobile-dropdown").animate({width: 'toggle'});
+        // }
+                  
+    });
 });
